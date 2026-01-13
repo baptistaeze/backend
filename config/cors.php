fd8cd200bc5577e7a9f6dc2b8410fe24977f7fa5
Ezequiel Baptista <ebaptista@trackingpremium.com>
2026-01-13 17:11:11 -0400

    fix: remove trailing slash from allowed origin URL in CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://frontend2-psi-two.vercel.app/',
+        'https://frontend2-psi-two.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
