7223fbb9581caf108c9c2ed04bd5b9d44b3d6886
Ezequiel Baptista <ebaptista@trackingpremium.com>
2026-01-13 16:53:47 -0400

    cors error

--- config/cors.php
+++ config/cors.php
@@ -21,6 +21,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'https://frontend2-nine-rho.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
